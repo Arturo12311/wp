6c602384efb5c15b38a1a483cee1d67f001f9b28
Arturo <liartur19@gmail.com>
2024-07-30 14:37:51 -0400

    added comments

--- packet.py
+++ packet.py
@@ -7,20 +7,30 @@ todo:
 
 # from msg import Msg
 from struct import unpack
-import json
+from json import load
 import os
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 """PACKET CLASS"""
 class Packet:
+    """
+    extracts, logs, and potentially modifies packet data
+    """
     def __init__(self, header_bytes, payload_bytes, operation):
+        # header data
         self.header_data = self.read_header(header_bytes, operation)
+
+        # payload data
         self.payload_data = self.read_payload(payload_bytes)
 
+
     def read_header(self, header_bytes, operation):
+        # import names file 
         full_path = os.path.join(CURRENT_DIR, "assets/names.json")
         with open(full_path, 'r') as f:
-            names = json.load(f)
+            names = load(f)
+
+        # header data if noname
         op = unpack("<I", header_bytes[17:21])[0]
         if str(op) not in names:
             header_data = {
@@ -32,22 +42,30 @@ class Packet:
                 "hash": unpack("!BBBB", header_bytes[8:12]),
                 "count": unpack("<I", header_bytes[13:17])[0]
             }
-        else:
-            header_data = {
-                "name": names[str(unpack("<I", header_bytes[17:21])[0])],
-                "type": operation,
-                "length": unpack("<I", header_bytes[4:8])[0],
-                "hash": list(unpack("!BBBB", header_bytes[8:12])),
-                "count": unpack("<I", header_bytes[13:17])[0]
-            }
+            return header_data
+
+        # regular header data
+        header_data = {
+            "name": names[str(unpack("<I", header_bytes[17:21])[0])],
+            "type": operation,
+            "length": unpack("<I", header_bytes[4:8])[0],
+            "hash": list(unpack("!BBBB", header_bytes[8:12])),
+            "count": unpack("<I", header_bytes[13:17])[0]
+        }
         return header_data
     
+
     def read_payload(self, payload_bytes):
+        # import structures file
         full_path = os.path.join(CURRENT_DIR, "assets/structs.json")
         with open(full_path, 'r') as f:
-            structs = json.load(f)
+            structs = load(f)
+
+        # get struct
         name = self.header_data["name"]
         structure = "unknown" if name == "unknown" else structs[name] 
+
+        # payload data
         payload_data = {
             "bytes": list(payload_bytes),
             "struct": structure,
@@ -55,6 +73,7 @@ class Packet:
             # "rest": msg.rb 
         }
         return payload_data
+
 
     def print_to_console(self):
         # print endpoints
@@ -72,8 +91,10 @@ class Packet:
             print(f"    {k:<8}: {v}")
         print("-")
 
+
     def write_to_file(self):
         def _write(path):
+            # open file
             full_path = os.path.join(CURRENT_DIR, path)
             os.makedirs(os.path.dirname(full_path), exist_ok=True)
             with open(full_path, 'a') as f:
@@ -92,12 +113,15 @@ class Packet:
                     f.write(f"    {k:<8}: {v}\n")
                 f.write("-\n")
 
+        # noname
         if self.header_data["name"] == "unknown":
             _write("log/er_nonames.txt")
 
+        # remainder
         # if self.payload_data["rest"] != []:
         #     _write("log/er_remainder.txt")
 
+        # regular
         _write("log/log.txt")
 
     
